d2f4e647bfbc2db5f7a3b3dc440bb5a3602866ae
Kirah Cox <kirahanncox@gmail.com>
2025-09-29 18:18:08 -0600

    finished assignment

---
+++ table row management with form/wwwroot/js/site.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function() {
+    $('#row-form').submit(function(event) {
+        event.preventDefault();
+
+        let name = $('#name').val();
+        let age = $('#age').val();
+        let occupation = $('#occupation').val();
+
+        if(name.trim() != '' && age.trim() != '' && occupation.trim() != ''){
+            let newRow = `<tr>
+                            <td>${name}</td>
+                            <td>${age}</td>
+                            <td>${occupation}</td>
+                            <td><button class="delete-btn">Delete</button></td>
+                          </tr>`;
+            $('#dynamic-table tbody').append(newRow);
+        }
+
+        $('#row-form')[0].reset();
+    });
+
+    $(document).on('click', '.delete-btn', function() {
+        $(this).closest('tr').remove();
+    });
+});
